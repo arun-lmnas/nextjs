b4c94ba4b0753a93f4dd735f3758aade3fd1b130
Arunkumar Ganesan <67154864+arun-lmnas@users.noreply.github.com>
2021-09-28 23:03:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -20,7 +21,14 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
+        <div className="container max-w-4xl m-auto px-4 mt-20">
+        <Image
+          src="/desk.jpg"
+          alt="my desk"
+          width={1920 / 2}
+          height={1280 / 2}
+        />
+      </div>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
